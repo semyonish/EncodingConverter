cd02f84e68244e196f7f363a642b0ceb770df373
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2020-04-01 20:23:25 +0400

    Initial Commit

---
+++ EncodingConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  EncodingConverter
+//
+//  Created by Семён Ишханян on 01.04.2020.
+//  Copyright © 2020 Семён Ишханян. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
