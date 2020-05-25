805e4b58c291bf590c52d4e833808e8609d7869b
Semyon Ishkhanyan <semyon.ishkhanyan@gmail.com>
2020-05-25 17:52:50 +0400

    improvements in encoding conversion

--- EncodingConverter/EncodingModule.swift
+++ EncodingConverter/EncodingModule.swift
@@ -11,7 +11,7 @@ import UniversalDetector
 
 class FileEncoder {
     
-    static let supportedEncodings: [FileEncoding] = [ .utf8, .utf8withBOM]
+    static let supportedEncodings: [FileEncoding] = [ .utf8, .utf8withBOM, .ascii, .windows1251]
     
     // MARK: - Public methods
     
@@ -37,18 +37,49 @@ class FileEncoder {
     static func encode(file: URL, to newEncoding: FileEncoding) -> Bool {
         let oldEncoding = getEncoding(of: file)
         
+        if oldEncoding == newEncoding {
+            return true
+        }
+        
         switch (oldEncoding, newEncoding) {
+        case (.ascii, .utf8):
+            return true
+        case (.ascii, .utf8withBOM):
+            return BOMProcessor.addBom(to: file)
+        
+        case (.utf8, .ascii):
+            return false
         case (.utf8, .utf8withBOM):
             return BOMProcessor.addBom(to: file)
         
         case (.utf8withBOM, .utf8):
             return BOMProcessor.removeBOM(from: file)
+            
+        case (.utf8withBOM, _):
+            return encode(file: file, to: .utf8) && encode(file: file, to: newEncoding)
+        
+        case (_, .utf8withBOM):
+            return encode(file: file, to: .utf8) && encode(file: file, to: .utf8withBOM)
         
         default:
-            print("FileEncoder.encode failed call with oldEnc = \(oldEncoding.rawValue), newEnc = \(newEncoding.rawValue)")
-            return false
+            return encode(file: file, from: oldEncoding, to: newEncoding)
         }
     }
-
+    
+    static private func encode(file: URL, from oldEncoding: FileEncoding, to newEncoding: FileEncoding) -> Bool {
+        do {
+            let readData = try Data.init(contentsOf: file)
+            if let stringData = String(data: readData, encoding: oldEncoding.stringEncoding) {
+                try stringData.write(to: file, atomically: true, encoding: newEncoding.stringEncoding)
+            } else {
+                return false
+            }
+        }
+        catch {
+            return false
+        }
+        
+        return true
+    }
 }
 
--- EncodingConverter/FileEncoding.swift
+++ EncodingConverter/FileEncoding.swift
@@ -12,15 +12,31 @@ enum FileEncoding: String {
     case ascii = "US-ASCII"
     case utf8 = "UTF-8"
     case utf8withBOM = "UTF-8 with BOM"
-    case utf16LE = "UTF-16LE"
-    case utf16BE = "UTF-16BE"
-    case utf32LE = "UTF-32LE"
-    case utf32BE = "UTF-32BE"
-    case xMacCyrillic = "x-mac-cyrillic"
+//    case utf16 = "UTF-16"
+//    case utf16LE = "UTF-16LE"
+//    case utf16BE = "UTF-16BE"
+//    case utf32LE = "UTF-32LE"
+//    case utf32BE = "UTF-32BE"
+//    case xMacCyrillic = "x-mac-cyrillic"
     case windows1251 = "windows-1251"
     case identificationError = "FAILED"
     
     init (utf8WithBom bom: Bool) {
         self = bom ? .utf8withBOM : .utf8
+    }
+    
+    var stringEncoding: String.Encoding {
+        switch self {
+        case .ascii:
+            return .ascii
+        case .utf8:
+            return .utf8
+        case .utf8withBOM:
+            return .utf8
+        case .windows1251:
+            return .windowsCP1251
+        case .identificationError:
+            return .utf8
+        }
     }
 }
